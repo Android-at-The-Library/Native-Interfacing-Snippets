c44778865051b2c4121ca3086fefe40bea8d9d8d
gkielian <gregory.kielian@gmail.com>
2015-07-05 04:14:11 -0400

    created working compilescript -- remember to prefix library with lib

---
+++ COMPILESCRIPT.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+javac HelloWorld.java
+javah -jni HelloWorld
+gcc -shared -o libHelloWorld.so -fPIC HelloWorld.c -I$JAVA_HOME/include -I$JAVA_HOME/include/linux
+
+LD_LIBRARY_PATH=.
+export LD_LIBRARY_PATH
